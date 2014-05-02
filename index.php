f53cbf82a673843948001ed66117ba5735f0ced5
frenz <giuseppe@gtgames.it>
2014-05-02 05:53:52 +0200

    add home bottom skinlet

--- index.php
+++ index.php
@@ -7,10 +7,20 @@ require_once(realpath(dirname(__FILE__)).'/include/view/template/InitGraphic.php
 $main = new Skin();
 InitGraphic::getInstance()->createGraphic($main);
 
+
+
+$homeBottom = new Skinlet('home-bottom');
 $home = new Skinlet("home");
 
+
+
+$slider = new Content($sliderEntity, $imageSliderRelation, $imageEntity);
+$slider->forceSingle();
+$slider->setFilter('id', '2');
+$slider->apply($home, 'slider');
+
 $main->setContent('body', $home->get());
+$main->setContent('homeBottom', $homeBottom->get());
 
-$main->close();  
 
-?>
+$main->close();
